a1de666e7a804413aff18a722b82237f2014a0bb
kjaneeta <jzps0355@leeds.ac.uk>
2026-05-01 09:32:33 +0000

    task3 pfunc

--- session2/task3/p_func.c
+++ session2/task3/p_func.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 
 int add( int i, int j );
+int subtract(int i, int j);
+int multiply(int i, int j);
+
 
 /* 
  * main: creating and testing function pointers
@@ -13,9 +16,22 @@ int main( void ) {
 
     int num1=2, num2=3; // data for arguments
 
-    \\ compute the sum using the function - print out the operands and answer
+    // compute the sum using the function - print out the operands and answer
+    printf('Sum using function add(5,6): %d\n', add(5,6));
 
-    \\ compute the sum using the pointer - print out the operands and answer
+    // compute the sum using the pointer - print out the operands and answer
+    operation = &add;
+    printf('sum using pointer operation(7,9): %d\n', operation(7,9));
+
+    printf('Sum using function subtract(5,6): %d\n', subtract(5,6));
+    operation = &subtract;
+    printf('difference using pointer operation(7,9): %d\n', operation(7,9));
+
+    printf('Sum using function multiply(5,6): %d\n', multiply(5,6));
+    operation = &multiply;
+    printf('product using pointer operation(7,9): %d\n', operation(7,9));
+
+
 
     return 0;
 }
@@ -26,4 +42,12 @@ int main( void ) {
 
 int add( int i, int j ) {
     return i+j;
+}
+
+int subtract( int i, int j ) {
+    return i-j;
+}
+
+int multiply( int i, int j ) {
+    return i*j;
 }
