9aa6bbf16a14edd9ecb0876b948dcd82522a7a18
kjaneeta <jzps0355@leeds.ac.uk>
2026-05-01 09:40:51 +0000

    pfunc done!

--- session2/task3/p_func.c
+++ session2/task3/p_func.c
@@ -17,21 +17,19 @@ int main( void ) {
     int num1=2, num2=3; // data for arguments
 
     // compute the sum using the function - print out the operands and answer
-    printf('Sum using function add(5,6): %d\n', add(5,6));
+    printf("Sum using function add(5,6): %d\n", add(5,6));
 
     // compute the sum using the pointer - print out the operands and answer
     operation = &add;
-    printf('sum using pointer operation(7,9): %d\n', operation(7,9));
+    printf("sum using pointer operation(7,9): %d\n", operation(7,9));
 
-    printf('Sum using function subtract(5,6): %d\n', subtract(5,6));
+    printf("Sum using function subtract(5,6): %d\n", subtract(5,6));
     operation = &subtract;
-    printf('difference using pointer operation(7,9): %d\n', operation(7,9));
+    printf("difference using pointer operation(7,9): %d\n", operation(7,9));
 
-    printf('Sum using function multiply(5,6): %d\n', multiply(5,6));
+    printf("Sum using function multiply(5,6): %d\n", multiply(5,6));
     operation = &multiply;
-    printf('product using pointer operation(7,9): %d\n', operation(7,9));
-
-
+    printf("product using pointer operation(7,9): %d\n", operation(7,9));
 
     return 0;
 }
